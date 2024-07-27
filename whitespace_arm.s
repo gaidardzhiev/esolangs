09ab0960600b84eeb878bb0d1b82d1fb7869d013
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-07-27 13:34:00 +0300

    Update whitespace_arm.s

--- whitespace_arm.s
+++ whitespace_arm.s
@@ -1,15 +1,3 @@
-	.arch armv7-a
-	.fpu neon
-	.eabi_attribute 28, 1
-	.eabi_attribute 20, 1
-	.eabi_attribute 21, 1
-	.eabi_attribute 23, 3
-	.eabi_attribute 24, 1
-	.eabi_attribute 25, 1
-	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
-	.eabi_attribute 34, 1
-	.eabi_attribute 18, 4
 	.text
 	.global	stack
 	.bss
